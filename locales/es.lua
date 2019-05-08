f123d1e9614b5eeb2d2bf41c32a852d8d6b86077
renildomarcio <krsolucoesweb@gmail.com>
2019-05-08 14:25:25 -0300

    Update #1.3.0 - 08-05-2019
    -Atualiação total dos arquivos do shop!

--- locales/es.lua
+++ locales/es.lua
@@ -1,9 +1,12 @@
 Locales['es'] = {
-
-	['shop'] = 'Tienda',
-	['shops'] = 'Tiendas',
-	['press_menu'] = 'Presiona ~INPUT_CONTEXT~ para acceder a la tienda.',
-	['bought'] = 'Has comprado ~b~1 ',
-	['not_enough'] = 'No tienes ~r~suficiente ~s~ dinero.',
-
+  ['shop'] = 'tienda',
+  ['shops'] = 'tiendas',
+  ['press_menu'] = 'pulsa ~INPUT_CONTEXT~ para comprar en la tienda.',
+  ['shop_item'] = '%s€',
+  ['bought'] = 'has comprado ~y~%sx~s~ ~b~%s~s~ por ~r~%s€~s~',
+  ['not_enough'] = 'no tienes ~r~suficiente ~s~ dinero: %s',
+  ['player_cannot_hold'] = 'no tienes ~y~espacio libre~s~ en tu inventario...',
+  ['shop_confirm'] = '¿Comprar %sx %s por $%s?',
+  ['no'] = 'no',
+  ['yes'] = 'si',
 }

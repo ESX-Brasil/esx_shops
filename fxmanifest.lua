fdcbe64b5897751d874758957db42956c7d47c66
renildomarcio <krsolucoesweb@gmail.com>
2020-05-11 19:07:32 -0300

    Update 1.1.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,30 +1,38 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
 
 description 'ESX Shops'
 
-version '1.3.0'
+version '1.1.1'
 
 client_scripts {
 	'@es_extended/locale.lua',
 	'locales/de.lua',
 	'locales/br.lua',
 	'locales/en.lua',
+	'locales/fi.lua',
 	'locales/fr.lua',
 	'locales/es.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'client/main.lua'
 }
 
 server_scripts {
 	'@es_extended/locale.lua',
+	'@mysql-async/lib/MySQL.lua',
 	'locales/de.lua',
 	'locales/br.lua',
 	'locales/en.lua',
+	'locales/fi.lua',
 	'locales/fr.lua',
 	'locales/es.lua',
 	'locales/sv.lua',
-	'@mysql-async/lib/MySQL.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'server/main.lua'
 }
+
+dependency 'es_extended'
